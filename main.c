de31d8dd8bbf029ce2d863c747a17e98852b36f3
Justin Davila <jadavila9@gmail.com>
2026-01-05 12:49:50 -0500

    first

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum
+{
+  OBJ_INT,
+  OBJ_PAIR
+} ObjectType;
+
+typedef struct sObject
+{
+  ObjectType type;
+
+  union
+  {
+    //OBJ_INT
+    int value;
+
+    //OBJ_PAIR
+    struct
+    {
+      struct sObject* head;
+      struct sObject* tail;
+    };
+  };
+} Object;
+
+
+int main()
+{
+  return 0;
+}
