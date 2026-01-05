24057369a5b47a801c3e8b38560f02dcdabd72b1
Justin Davila <jadavila9@gmail.com>
2026-01-05 13:10:04 -0500

    feat: create push/pop functions to stack based on size of stack

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #define STACK_MAX 256
 
@@ -38,6 +39,18 @@ VM* newVM()
   VM* vm = malloc(sizeof(VM));
   vm->stackSize = 0;
   return vm;
+}
+
+void push(VM* vm, Object* value)
+{
+  assert(vm->stackSize < STACK_MAX, "Stack overflow");
+  vm->stack[vm->stackSize++] = value;
+}
+
+Object* pop(VM* vm)
+{
+  assert(vm->stackSize > 0, "Stack underflow");
+  return vm->stack[--vm->stackSize];
 }
 
 int main()
